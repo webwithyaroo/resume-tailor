6e59c472a29b4ea5d6515ce46f218dd8bf3f3b0e
yaroo <olawaleridollahi@gmail.com>
2026-04-12 08:55:16 +0100

    feat: extract extra and matched phrases and keywords

--- app.py
+++ app.py
@@ -5,72 +5,72 @@ from matcher import match_resume_to_job
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    
+
     if request.method == "POST":
-        
-        job = request.form.get('job') 
-        resume = request.form.get('resume') 
-        
-        
-        if not job or not resume :
+
+        job = request.form.get('job')
+        resume = request.form.get('resume')
+
+        if not job or not resume:
             return "Please provide your resume and job description"
-        
+
         print("Resume started")
-        
-        
+
         # resume actions
-        resume_processor = resume_tailor(resume_details = resume)
+        resume_processor = resume_tailor(resume_details=resume)
         resume_keywords = resume_processor["keywords"]
-        resume_phrases = resume_processor["phrases" ]
-        
-        
-        
-        print("+" *40)
+        resume_phrases = resume_processor["phrases"]
+
+        print("+" * 40)
         print("Job started")
-        
+
         # job actions
-        job_processor = job_description(job_details = job)
+        job_processor = job_description(job_details=job)
         job_keywords = job_processor["keywords"]
         job_phrases = job_processor["phrases"]
-        
-        
+
         # missing = match_resume_to_job(resume_processor, job_processor)
-        missing = match_resume_to_job(resume_keywords, resume_phrases, job_keywords,job_phrases
-)
-        print("+" *40)
-        
-        
+        missing = match_resume_to_job(resume_keywords, resume_phrases, job_keywords, job_phrases
+                                      )
+        print("+" * 40)
+
+
+        #################### missing keywords and phrases ##########################################
         print("Missing keywords:")
-        print(missing["keywords"])
+        print(missing["missing_keywords"])
+        print(missing["missing_phrases"])
         
-        print("Missing phrases:")
-        print(missing["phrases"])
+        ################################ matched keywords and phrases ##########################################
+        print("Matched keywords:")
+        print(missing["matched_keywords"])
+        print(missing["matched_phrases"])
         
+        ########################################## extracted keywords and phrases ##########################################
+        print("Extra keywords:")
+        print(missing["extra_keywords"])
+        print(missing["extra_phrases"])
 
         # missing phrases
         phrases_html = ""
-        if missing['phrases']:
+        if missing['missing_phrases']:
             phrases_html = f"""
             <h2>Missing phrases:</h2>
             <ul>
-                {"".join(f"<li>{phrase}</li>" for phrase in missing['phrases'])}
+                {"".join(f"<li>{phrase}</li>" for phrase in missing['missing_phrases'])}
             </ul>"""
-        
-        
-        # missing keywords 
+
+        # missing keywords
         keywords_html = ""
-        if missing['keywords']:
+        if missing['missing_keywords']:
             keywords_html = f"""
             <h2>Missing keywords:</h2>
             <ul>
-                {"".join(f"<li>{keyword}</li>" for keyword in missing['keywords'])}
+                {"".join(f"<li>{keyword}</li>" for keyword in missing['missing_keywords'])}
             </ul>"""
-        
-        
-        
-        
+
         return f"""
             <h2>Resume received</h2>
             <p>{resume[:50]} .....</p>
@@ -82,9 +82,8 @@ def home():
                 
 
         """
-    
-    return  render_template("form.html")
 
+    return render_template("form.html")
 
 
 if __name__ == '__main__':
--- matcher.py
+++ matcher.py
@@ -1,28 +1,37 @@
 def match_resume_to_job(job_keywords, resume_keywords, job_phrases, resume_phrases) -> dict:
     """
     Compares the keywords from the resume and job description and returns the missing keywords.
-    
+
     Parameters:
     resume_keywords (list): A list of keywords extracted from the resume.
     job_keywords (list): A list of keywords extracted from the job description.
-    
+
     Returns:
     dict: A dictionary containing the missing keywords from the resume that are present in the job description.
     """
-    
-    
-    # extracting the keywords and phrases from the resume and job description
-    
+
+    # extracting the matching keywords and phrases from the resume and job
+    matched_keywords = set(resume_keywords) & set(job_keywords)
+    matched_phrases = set(resume_phrases) & set(job_phrases)
+
+    structured_matched_keywords = [*matched_keywords]
+    structured_matched_phrases = [*matched_phrases]
+
+    # extracting the extra keywords and phrases from the resume that are not in the job description
+    extra_keywords = set(resume_keywords) - set(job_keywords)
+    extra_phrases = set(resume_phrases) - set(job_phrases)
+
+    structured_extra_keywords = [*extra_keywords]
+    structured_extra_phrases = [*extra_phrases]
+
     # comparing the resume and job description keywords
     missing_keywords = set(job_keywords) - set(resume_keywords)
-    
-    # comparing the resume and job description phrases
     missing_phrases = set(job_phrases) - set(resume_phrases)
-    
-    # converting sets to lists for better display
-    converting_missing_keywords = [*missing_keywords]
-    converting_missing_phrases = [*missing_phrases]
-    
-    result = {"keywords": converting_missing_keywords, "phrases": converting_missing_phrases}
-    
+
+    structured_missing_keywords = [*missing_keywords]
+    structured_missing_phrases = [*missing_phrases]
+
+    result = {"missing_keywords": structured_missing_keywords, "missing_phrases": structured_missing_phrases, "extra_keywords": structured_extra_keywords,
+              "extra_phrases": structured_extra_phrases, "matched_keywords": structured_matched_keywords, "matched_phrases": structured_matched_phrases}
+
     return result
